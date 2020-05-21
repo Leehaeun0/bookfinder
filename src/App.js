c7ff45827ee6d03261b01587d946098d167ac3df
Leehaeun0 <a01050675111@gmail.com>
2020-05-21 19:41:11 +0900

    doc: setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Hellow World!</div>;
 }
 
 export default App;
