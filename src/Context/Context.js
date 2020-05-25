76369868d544b8db04959775b1a022e842cd9417
Leehaeun0 <a01050675111@gmail.com>
2020-05-25 09:11:21 +0900

    setting: set sass and merge from feature/context

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -6,7 +6,7 @@ export const BookContext = React.createContext();
 
 export function ContextProvider({ children }) {
   // const [] = UseFetch();
-  const contextValue = ['test'];
+  const contextValue = ['test1'];
   return (
     <BookContext.Provider value={contextValue}>{children}</BookContext.Provider>
   );
