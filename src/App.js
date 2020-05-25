2877aa379ebc96a2eaaec805224915dabc8b0771
Leehaeun0 <a01050675111@gmail.com>
2020-05-25 09:38:14 +0900

    merge: merge useFetch from chiwon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Header from './Components/Header';
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
-=======
 import React, { useContext } from 'react';
 import { BookContext } from './Context/Context';
 import './App.css';
@@ -17,7 +6,6 @@ function App() {
   const bookContext = useContext(BookContext);
   console.log(bookContext);
   return <div className="App">test</div>;
->>>>>>> 41f4d221b5b91dcac5f65de8eb92071ac6c4a7ae
 }
 
 export default App;
