ebebd4d20883c2fc45ed7c2ee93bcb99ed0455d0
Mang-developer <chiwon618@gmail.com>
2020-05-25 15:59:19 +0900

    For pull

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { useContext } from 'react';
-import { BookContext } from './Context/Context';
 import './login.scss';
 import { Link } from 'react-router-dom';
 
 function App() {
-  const bookContext = useContext(BookContext);
   return (
     <div className="App">
       <Link to="/login">로그인</Link>
